e57b3e7c1e763430dbfc8f309c226da5151b8ecf
Scott Novak <scottsman@gmail.com>
2013-11-19 02:22:29 +0000

    Updating docstrings.

--- ratingWidget.py
+++ ratingWidget.py
@@ -4,17 +4,25 @@ from PyQt4 import QtCore, QtGui
 
 
 class RatingWidget(QtGui.QWidget):
-    """
+    """ A QWidget that enables a user to choose a rating.
     """
 
     def __init__(self, parent=None, icon_path=None, num_icons=5):
-        """
+        """Constructor
+
+        Kwargs:
+            parent (QtCore.QObject): Parent of the RatingWigdet.
+            icon_path (str): The location of the icon used to represent one value of a
+                rating.
+            num_icons (int): The number of icons the RatingWidget will display.
         """
         super(RatingWidget, self).__init__(parent)
 
+        # Defaults
         self.value = 0
         self.max_value = num_icons
 
+        # Fallback for the icon_path.
         if not icon_path:
             icon_path = os.path.join(os.path.dirname(__file__), 'rating.png')
 
@@ -36,6 +44,8 @@ class RatingWidget(QtGui.QWidget):
         self.installEventFilter(self)
 
     def setActiveIconsVisible(self):
+        """ Display any icons that are active.
+        """
         for icon in self.icons:
             if icon.active:
                 icon.setImage(True)
@@ -43,6 +53,20 @@ class RatingWidget(QtGui.QWidget):
                 icon.setImage(False)
 
     def setIconsActive(self, icon_label, active):
+        """ Update the icons active state.
+
+        All icons less and equal to the value of the icon_label have their active status
+        set to True. Everything higher than the icon_label have their active
+        status set to False.
+        eg. If icon_label 3 is passed in, then 1, 2 and 3 are active
+           and 4 and 5 are innactive. 
+
+        If active is False then all icons visibility is reset according to their
+        active status.
+
+        Args:
+            icons_label (IconLabel): The icon to update to.
+        """
         if active:
             self.value = icon_label.value
             for icon in self.icons:
@@ -56,6 +80,8 @@ class RatingWidget(QtGui.QWidget):
             self.setActiveIconsVisible()
 
     def setIconsVisible(self, icon_label, visible):
+        """ Update the icons visibility
+        """
         if visible:
             for icon in self.icons:
                 if icon.value <= icon_label.value:
@@ -85,6 +111,7 @@ class IconLabel(QtGui.QLabel):
 
         self.image_path = image_path
         self.parent = parent
+        # Holds the state determining if the Icon is being used.
         self.active = False
         self.value = value
 
