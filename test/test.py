44d994b715bd30e4f675e60a90582a0e9c4ed5a9
Scott Novak <scottsman@gmail.com>
2013-12-18 17:41:06 +0000

    Adding tets for IconLabel.

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import unittest
@@ -116,10 +117,90 @@ class TestRatingWidget(unittest.TestCase):
         for count, icon in enumerate(rating_widget.icons):
             self.assertEqual(icon.visible, bool(count < limit))
 
+    def test_max_value(self):
+        """Assert that the max_value is set correctly when setting the number
+        of icons for the rating widget.
+        """
+        max_value = 10
+
+        rating_widget = ratingWidget.RatingWidget(num_icons=max_value)
+
+        self.assertEqual(rating_widget.max_value, max_value)
+
+    def test_value(self):
+        """Assert that the value of the rating_widget is updated and retreivable.
+        """
+        rating_widget = ratingWidget.RatingWidget(num_icons=5)
+        self.assertEqual(rating_widget.value, 0)
+
+        test_icon = rating_widget.icons[2]
+        rating_widget.set_icons_active(test_icon)
+        self.assertEqual(rating_widget.value, test_icon.value)
+
+        test_icon = rating_widget.icons[4]
+        rating_widget.set_icons_active(test_icon)
+        self.assertEqual(rating_widget.value, test_icon.value)
+
+
 class TestIconLabel(unittest.TestCase):
 
-    def test_num_icons(self):
-        pass
+    _icon_path = os.path.join(os.path.dirname(__file__), '..', 'media', 'rating.png')
+
+    def setUp(self):
+        """Create a QApplication.
+        """
+        self.app = QtGui.QApplication(sys.argv)
+        QtGui.qApp = self.app
+        self.icon_label = ratingWidget.IconLabel(self._icon_path, 1)
+
+    def tearDown(self):
+        """Remove the QApplication
+        """
+        self.icon_label = None
+        QtGui.qApp = None
+        self.app = None
+
+    def test_init(self):
+        self.assertFalse(self.icon_label.active)
+        self.assertFalse(self.icon_label.visible)
+        self.assertEqual(self.icon_label.value, 1)
+
+    def test_set_get_active(self):
+        self.icon_label.active = True
+        self.assertTrue(self.icon_label.active)
+
+    def test_set_get_visible(self):
+        self.icon_label.visible = True
+        self.assertTrue(self.icon_label.visible)
+
+    def test_set_image_not_set(self):
+        self.assertFalse(self.icon_label.visible)
+
+    def test_set_image_false(self):
+        self.icon_label.set_image(False)
+        self.assertFalse(self.icon_label.visible)
+
+    def test_set_image_true(self):
+        self.icon_label.set_image(True)
+        self.assertTrue(self.icon_label.visible)
+
+    # Signal testing
+    def assert_icon_emitted(self, icon_emitted):
+        self.assertEqual(icon_emitted, self.icon_label)
+
+    def test_eventFilter_enter(self):
+        self.icon_label.mouse_enter_icon.connect(self.assert_icon_emitted)
+        self.icon_label.eventFilter(self.icon_label, QtCore.QEvent(QtCore.QEvent.Enter))
+
+    def test_eventFilter_leave(self):
+        self.icon_label.mouse_leave_icon.connect(self.assert_icon_emitted)
+        self.icon_label.eventFilter(self.icon_label, QtCore.QEvent(QtCore.QEvent.Leave))
+
+    def test_eventFilter_mouseRelease(self):
+        self.icon_label.mouse_release_icon.connect(self.assert_icon_emitted)
+        QTest.mouseRelease(self.icon_label, QtCore.Qt.LeftButton)
+
+
 
 if __name__ == "__main__":
     unittest.main()
